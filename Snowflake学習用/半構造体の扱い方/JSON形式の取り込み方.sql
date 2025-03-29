39dbfbd9d2142bb46a03279c447110a30c7f6443
RYOTA2528 <1216022@gmail.com>
2025-03-29 11:09:37 +0900

    [ADD]Snowflakeの学習範囲更新(JSON形式の取り込み方を学ぶ)

---
+++ Snowflake学習用/半構造体の扱い方/JSON形式の取り込み方.sql
@@ -0,0 +1,31 @@
+// JSON DDL スクリプト
+use database library_card_catalog; 
+
+// JSON データの取り込みテーブルを作成する
+create or replace table library_card_catalog.public.author_ingest_json
+(
+  raw_author variant
+);
+
+
+// JSONデータを読み込むためのファイル形式を作成する
+create or replace file format library_card_catalog.public.json_file_format
+type = 'JSON' 
+compression = 'AUTO' 
+enable_octal = false
+allow_duplicate = false
+strip_outer_array = true --trueにすることで角括弧を無視して各著者を別々の行に読み込むことができる
+strip_null_values = false 
+ignore_utf8_errors = false; 
+
+//手動で作成したステージ(@json_input)から実際にauthor_ingest_jsonテーブルへ読み込む
+COPY INTO author_ingest_json
+from @json_input
+FILE_FORMAT = (FORMAT_NAME = library_card_catalog.public.json_file_format);
+
+// 取得項目の確認
+select * from author_ingest_json;
+
+
+//raw_authorのデータを確認
+select raw_author from author_ingest_json;
