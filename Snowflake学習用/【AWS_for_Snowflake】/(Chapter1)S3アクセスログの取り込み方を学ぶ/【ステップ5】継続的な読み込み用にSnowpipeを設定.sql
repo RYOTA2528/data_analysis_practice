5fbe610c7df62fc5be13f909f5da029657843cf1
RYOTA2528 <1216022@gmail.com>
2025-04-22 22:06:34 +0900

    [ADD]Snowpipwの構成並びにAWS SQSとの連携のためのSQS のARNを確認

---
+++ Snowflake学習用/【AWS_for_Snowflake】/(Chapter1)S3アクセスログの取り込み方を学ぶ/【ステップ5】継続的な読み込み用にSnowpipeを設定.sql
@@ -0,0 +1,16 @@
+// Snowflake Snowpipeを構成する
+create pipe public.s3_access_logs_pipe auto_ingest=true as
+  copy into s3_access_logs_staging from (
+    SELECT
+      STG.$1,
+      current_timestamp() as timestamp
+    FROM @s3_access_logs (FILE_FORMAT => TEXT_FORMAT) STG
+  );
+  
+  
+// 自動データ取り込み（Snowpipe）に関連するパイプの一覧が表示されます。
+-- この中には、SQS連携などで必要になる SQSキュー ARN も含まれています。
+-- notification_channel SQSキューの ARN（連携用）
+-- この ARN を使って、S3 → SQS → Snowpipe の連携設定を AWS 側に組み込みます（イベント通知の送信先として）
+  SHOW PIPES;
+  
