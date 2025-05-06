aef2bba676e550b2785b8d5d4d3f6d3846cac86f
RYOTA2528 <1216022@gmail.com>
2025-05-06 12:21:16 +0900

    [ADD]先に作成した PYTHON_API_TABLE テーブルの詳細を取得および確認

--- Snowflake学習用/【Snowflake Python APIs】/(Chapter4)データベース、スキーマ、テーブルの作成.py
+++ Snowflake学習用/【Snowflake Python APIs】/(Chapter4)データベース、スキーマ、テーブルの作成.py
@@ -37,3 +37,38 @@ table = schema.tables.create(
   ),
 mode=CreateMode.or_replace
 )
+
+-------------------------------------------------------
+④先に作成した PYTHON_API_TABLE テーブルの詳細を取得する
+
+table_details = table.fetch()
+
+# 出来上がったオブジェクトに対して .to_dict() を呼び出すと、その詳細情報を表示することができます。
+table_details.to_dict()
+
+# ノートブックは、このような PYTHON_API_TABLE テーブルに関するメタデータを含むディクショナリを表示
+{
+    "name": "PYTHON_API_TABLE",
+    "kind": "TABLE",
+    "enable_schema_evolution": False,
+    "change_tracking": False,
+    "data_retention_time_in_days": 1,
+    "max_data_extension_time_in_days": 14,
+    "default_ddl_collation": "",
+    "columns": [
+        {"name": "TEMPERATURE", "datatype": "NUMBER(38,0)", "nullable": False},
+        {"name": "LOCATION", "datatype": "VARCHAR(16777216)", "nullable": True},
+    ],
+    "created_on": datetime.datetime(
+        2024, 5, 9, 8, 59, 15, 832000, tzinfo=datetime.timezone.utc
+    ),
+    "database_name": "PYTHON_API_DB",
+    "schema_name": "PYTHON_API_SCHEMA",
+    "rows": 0,
+    "bytes": 0,
+    "owner": "ACCOUNTADMIN",
+    "automatic_clustering": False,
+    "search_optimization": False,
+    "owner_role_type": "ROLE",
+}
+
