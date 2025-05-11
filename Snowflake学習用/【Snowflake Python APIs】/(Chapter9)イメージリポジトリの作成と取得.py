19011aeb7e3c04b7f27023836f903427618aebb5
RYOTA2528 <1216022@gmail.com>
2025-05-11 12:17:44 +0900

    [ADD]リポジトリの詳細を取得し、その名前を表示することで、リポジトリが正常に作成されたことを確認

--- Snowflake学習用/【Snowflake Python APIs】/(Chapter9)イメージリポジトリの作成と取得.py
+++ Snowflake学習用/【Snowflake Python APIs】/(Chapter9)イメージリポジトリの作成と取得.py
@@ -11,3 +11,11 @@ my_repo = ImageRepository("MyImageRepository")
 schema.image_repositories.create(my_repo)
 
 # で以前に作成したデータベースとスキーマにイメージリポジトリを作成
+
+# 2 リポジトリの詳細を取得し、その名前を表示することで、リポジトリが正常に作成されたことを確認する
+my_repo_res = schema.image_repositories["MyImageRepository"]
+my_repo = my_repo_res.fetch()
+print(my_repo.name)
+
+
+# 3 
