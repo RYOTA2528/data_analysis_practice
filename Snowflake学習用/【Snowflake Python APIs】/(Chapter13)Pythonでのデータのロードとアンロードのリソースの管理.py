465defb7c4642e5f3788a72a5f49c43e1bf5c4d0
RYOTA2528 <1216022@gmail.com>
2025-05-11 18:11:14 +0900

    [ADD]クラウドストレージ内のデータファイルの場所であるSnowflakeステージ操作

--- Snowflake学習用/【Snowflake Python APIs】/(Chapter13)Pythonでのデータのロードとアンロードのリソースの管理.py
+++ Snowflake学習用/【Snowflake Python APIs】/(Chapter13)Pythonでのデータのロードとアンロードのリソースの管理.py
@@ -18,3 +18,37 @@ my_stage = Stage(
 stages = root.database["my_db"].schemas["my_schema"].stages
 stages.create(my_stage)
 
+# ステージ詳細の取得
+my_stage = root.database["my_db"].schemas["my_schema"].stages["my_stage"].fetch()
+print(my_stage.to_dict())
+
+# ステージの一覧表示(名前に my というテキストを含むステージを一覧表示)
+from snowflake.core.stage import StageCollection
+
+stages: StageCollection = root.database["my_db"].schemas["my_schema"].stages
+stage_iter = stages.iter(like="my%")
+for stage_obj in stage_iter:
+  print(stage_obj.name)
+
+
+# ステージの操作
+# Snowflake内のステージリソースにアクセス
+# データベース: my_db, スキーマ: my_schema, ステージ: my_stage
+my_stage_res = root.databases["my_db"].schemas["my_schema"].stages["my_stage"]
+
+# ローカルファイル './my-file.yaml' をステージにアップロード
+# "/" はステージ内のルートディレクトリを示す
+# auto_compress=False により自動圧縮なし
+# overwrite=True により同名ファイルがあれば上書き
+my_stage_res.put("./my-file.yaml", "/", auto_compress=False, overwrite=True)
+
+# ステージ内にあるファイルの一覧を取得
+stageFiles = root.databases["my_db"].schemas["my_schema"].stages["my_stage"].list_files()
+
+# アップロードされたファイル（およびその他のファイル）を表示
+for stageFile in stageFiles:
+    print(stageFile)  # 例: 'my-file.yaml'
+
+# ステージ自体を削除（中のファイルも含めてすべて消えるので注意！）
+my_stage_res.drop()
+
